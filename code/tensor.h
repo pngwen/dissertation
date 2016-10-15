20db7f0d15e2252f054a16ef0123105c7523fe91
root <root@localhost.localdomain>
2016-10-15 01:37:21 +0000

    syncing

--- code/tensor.h
+++ code/tensor.h
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * File: tensor.h
  * Purpose: Declaration of a tensor class.  
  * Author: Robert Lowe
@@ -22,45 +22,46 @@ public:
     //create an index for an n-dimensioned tensor (initialized to -1)
     TensorIndex(int n) : v(n, -1) { /* all done! */ }
     
+    //copy constructor
+    TensorIndex(const TensorIndex &rhs) : v(rhs.v.begin(), rhs.v.end()) { /* nothing */ }
+    
     //provides access to the index element
     int & operator[](int i) { return v[i]; }
+    
+    //comparison operations
+    bool operator<(const TensorIndex& rhs) { return compare(rhs) < 0; }
+    bool operator==(const TensorIndex& rhs) { return compare(rhs) == 0; }
+    bool operator>(const TensorIndex& rhs) { return compare(rhs) > 0; }
+    
 protected:
     std::vector<int> v;
+    
+    int compare(const TensorIndex &rhs)
+    {
+        for(int i=0; i<v.size(); i++) {
+            if(v[i]>rhs.v[i]) return 1;
+            if(v[i]<rhs.v[i]) return -1;
+        }
+        //must be equal
+        return 0;
+    }
 };
 typedef TensorIndex TensorDimension;
 
 
-class TensorBase
+class Tensor
 {
 public:
-    virtual operator double &() { throw std::bad_cast(); }
-    virtual double & operator=(double rhs) { throw std::bad_cast(); }
-    virtual TensorBase &operator[](int i) = 0;
-};
-
-
-class Tensor : TensorBase
-{
-public:
-    Tensor(std::vector<int> dims);
+    Tensor(const TensorDimension & dims);
     virtual ~Tensor();
-   
-    //index operator works one dimension at a time
-    virtual TensorBase &operator[](int i);
+    
+    //used as part of the general index strategy
     
 protected:
-    std::map<int,TensorBase*> elements;
-    std::vector<int> d;
+    std::map<int,double> elements;
+    TensorDimension d;
+    
 };
 
 
-class TensorScalar : public TensorBase
-{
-public:
-    virtual operator double&() { return value; }
-    virtual double & operator=(double rhs) { value=rhs; } 
-    virtual TensorBase &operator[](int i) {throw std::out_of_range("Attempted index on scalar object."); }
-protected:
-    double value;
-};
 #endif
